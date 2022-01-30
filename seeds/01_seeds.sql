f45c2d19396887053ebaf24ddbed93223ec5bc1f
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-01-30 17:30:54 +0000

    Insert seeds into users table

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,1 +1,9 @@
-INSERT INTO users (name, email, password)
+INSERT INTO users (name, email)
+VALUES('Eva Stanley', 'sebastianguerra@ymail.com'),
+('Louisa Meyer', 'jacksonrose@hotmail.com'),
+('Dominic Parks', 'victoriablackwell@outlook.com'),
+('Sue Luna', 'jasonvincent@gmx.com'),
+('Rosalie Garza', 'jacksondavid@gmx.com'),
+('Etta West', 'charlielevy@yahoo.com'),
+('Margaret Wong', 'makaylaweiss@icloud.com'),
+('Leroy Hart', 'jaycereynolds@inbox.com');
