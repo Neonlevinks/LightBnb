491f8a861a9190ae765515b86b7a34db02e07f44
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-01-30 17:00:45 +0000

    Added properties table

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -3,4 +3,22 @@ CREATE TABLE users (
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
-)
+);
+
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY,
+  owner_id INTEGER,
+  title VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  thumbnail VARCHAR(255) NOT NULL,
+  cover_photo_url VARCHAR(255) NOT NULL,
+  cost_per_night INTEGER NOT NULL,
+  parking_spaces INTEGER,
+  number_of_bathrooms INTEGER,
+  number_of_bedrooms INTEGER,
+  country VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  province VARCHAR(255) NOT NULL,
+  post_code VARCHAR(255) NOT NULL,
+  active BOOLEAN
+);
